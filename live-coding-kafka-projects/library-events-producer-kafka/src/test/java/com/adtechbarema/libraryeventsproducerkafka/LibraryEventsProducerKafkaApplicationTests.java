3c4929f82a38b6596b1ff55ee314f99dfba72552
addisu mekonnen marilign <addisumekonnenmarilign@Addisus-MBP.home>
2023-06-08 11:11:01 -0500

    library-event-producer-project-setup

---
+++ live-coding-kafka-projects/library-events-producer-kafka/src/test/java/com/adtechbarema/libraryeventsproducerkafka/LibraryEventsProducerKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adtechbarema.libraryeventsproducerkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsProducerKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
